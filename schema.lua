7f104f0e905fc9321f94738fb3632667b0f31be5
Christian Chostak <c.chostak@gmail.com>
2018-05-28 11:05:52 +0100

    Alterado schema.lua

--- schema.lua
+++ schema.lua
@@ -1,11 +1,10 @@
 local utils = require "kong.tools.utils"
 
-
 return {
   no_consumer = true,
   strip_path = true,
   fields = {
-    key_names = {type = "array", required = true, default = {"ctk"}},
+    key_names = {type = "array", required = true, default = {"ctk2"}},
     url = {type = "url", default = "", required = true}
   },
 }
